9ba0fa9bbdc2abdf735f94e5bd4835c05579baa6
JPuyear <jpuyear@colostate.edu>
2025-02-27 07:32:23 -0700

    save save save

---
+++ 08-exercise/day-08.R
@@ -0,0 +1,50 @@
+#Josh Puyear
+#02/24/2025
+#Practice making joins and pivots
+
+# Make a faceted plot of the cumulative cases & 
+#deaths by USA region. Your x axis should be the
+# date and the y axis value/count. To do this you
+# will need to join and pivot the COVID-19 data.
+# We can break this task into 7 steps:
+
+#Read in the COVID-19 Data
+
+
+
+#Create a new data.frame using the available 
+#state.abb, state.name, state.region objects in 
+# base R. Be intentional about creating a primary 
+#key to match to the COVID data!
+
+
+# example code
+#df = data.frame(region = state.region,
+#                ...,
+#                ...)
+
+
+
+#Join your new data.frame to the raw COVID data.
+#Think about right, inner, left, or full join…
+
+
+
+# split-apply the joined data to determine the daily,
+# cumulative, cases and deaths for each region
+
+
+
+# Pivot your data from wide format to long
+
+
+
+# Plot your data in a compelling way (setup, layers,
+# labels, facets, themes)
+
+
+
+# Save the image to your img directory with a good
+# file name and extension!
+
+
