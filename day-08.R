750689b5caaf74987f54f16cc7dc4e8b24191428
Josh Puyear <jpuyear@colostate.edu>
2025-04-08 10:35:59 -0600

    added index.qmd

--- day-08.R
+++ day-08.R
@@ -2,21 +2,14 @@
 #02/24/2025
 #Practice making joins and pivots
 
-# Make a faceted plot of the cumulative cases & 
-#deaths by USA region. Your x axis should be the
-# date and the y axis value/count. To do this you
-# will need to join and pivot the COVID-19 data.
-# We can break this task into 7 steps:
+Make a faceted plot of the cumulative cases & deaths by USA region. Your x axis should be the date and the y axis value/count. To do this you will need to join and pivot the COVID-19 data. We can break this task into 7 steps:
 
 #Read in the COVID-19 Data
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
 covid = read_csv(url)
 head(covid, 5)
 
-#Create a new data.frame using the available 
-#state.abb, state.name, state.region objects in 
-# base R. Be intentional about creating a primary 
-#key to match to the COVID data!
+#Create a new data.frame using the available state.abb, state.name, state.region objects in  base R. Be intentional about creating a primary key to match to the COVID data!
 
 
 # example code
@@ -31,8 +24,7 @@ head(df)
 
 
 
-#Join your new data.frame to the raw COVID data.
-#Think about right, inner, left, or full join…
+#Join your new data.frame to the raw COVID data. Think about right, inner, left, or full join…
 
 covid_region <- inner_join(df, covid, by = "state") |>
   group_by(region, date) |>  
